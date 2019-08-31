2d3de300a5a1c0ed557adc9b7cfb69e4220910e3
Peter Darche <pdarche@gmail.com>
2019-08-31 14:04:49 -0700

    Add lock icon to security component

--- app/components/SecurityNotice.js
+++ app/components/SecurityNotice.js
@@ -3,23 +3,35 @@ import {
   StyleSheet,
   Text,
   View,
-  Dimensions
+  Dimensions,
+  Image
 } from 'react-native';
 import {
   widthPercentageToDP as wp
 } from 'react-native-responsive-screen';
 
-
 import { Button } from 'react-native-elements';
 import { Style } from '../config/styles';
+
 
 export default class SecurityNotice extends React.Component {
   render() {
     return (
       <View style={styles.security}>
-        <Text style={styles.securityText}>
-          This data is only stored on your phone. Learn more about our security here.
-        </Text>
+        <View style={styles.lock}>
+          <Image
+            source={require('../../assets/padlock.png')}
+            style={{
+              width: Dimensions.get('window').width * .05,
+              height: Dimensions.get('window').width * .05,
+            }}
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.securityText}>
+            This data is only stored on your phone. Learn more about our security here.
+          </Text>
+        </View>
       </View>
     );
   }
@@ -32,11 +44,24 @@ const styles = StyleSheet.create({
     paddingHorizontal: Style.margins.MEDIUM,
     justifyContent: 'flex-start',
     alignItems: 'center',
+    flexDirection: 'row'
+  },
+  textContainer: {
+    width: Dimensions.get('window').width * .8,
   },
   securityText: {
-    fontSize: wp('4%'),
+    fontSize: wp('3.5%'),
     fontFamily: Style.fonts.family.fontFamily,
     fontWeight: '100',
     color: Style.colors.DARKER_GRAY
-  }
+  },
+  lock: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: Dimensions.get('window').width * .04,
+    width: Dimensions.get('window').width * .1,
+    height: Dimensions.get('window').width * .1,
+    borderRadius: Dimensions.get('window').width * .05,
+    backgroundColor: Style.colors.VERY_LIGHT_GRAY2
+  },
 });
